a6498b367af6bbe4cc89b31e8e6bbbf4c2756810
ingracristina <ingra.cristina@amaro.com>
2022-11-26 15:45:43 -0400

    homePage

---
+++ cedomca/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <h1>Hey from HomePage</h1>
+                <p>This is your awesome HomePage subtitle</p>
+            <h1>Hey from HomePage</h1>
+                <p>This is your awesome HomePage subtitle</p>
+        </div>
+    );
+}
