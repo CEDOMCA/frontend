e3d80bf89b776c75f5a4404bd69064fc0ed954c4
guilherme <jguicf@gmail.com>
2022-11-27 13:23:00 -0400

    initial setup done

--- cedomca/src/pages/HomePage.js
+++ cedomca/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Button } from 'react-bootstrap';
 import { useState } from "react";
+import axios from "axios";
 
 export default function HomePage() {
     const [fullName, setName] = useState("");
@@ -10,61 +11,54 @@ export default function HomePage() {
     const [country, setCountry] = useState("");
     const [state, setState] = useState("");
     const [city, setCity] = useState("");
-    const role = "visitor"
+    const role = "visitor";
     const [message, setMessage] = useState("");
 
     let handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-          let res = await fetch("https://cedomca-backend.herokuapp.com/user", {
+        const options = {
             method: "POST",
-            body: JSON.stringify({
-              name: fullName,
-              birthDate: birthDate,
-              email: email,
-              password: password,
-              country: country,
-              state: state,
-              city: city,
-              role: "visitor"
-            }),
-          });
-          let resJson = await res.json();
-          if (res.status === 200) {
-            setName("")
-            setDate("")
-            setEmail("")
-            setPassword("")
-            setCountry("")
-            setState("")
-            setCity("")
-            setMessage("User created successfully");
-            console.log(res);
-          } else {
-            setMessage("Some error occured");
+            mode: "no-cors",
+            withCredentials: false,
+            body: {
+                fullName: fullName,
+                birthDate: birthDate,
+                email: email,
+                password, password,
+                country: country,
+                state: state,
+                city: city,
+                role: role
+            },
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*"
+            }
+          };
+          try {
+            let res = await axios.post("https://cedomca-backend.herokuapp.com/user", options);
+          } catch(err) {
+            console.log(err)
           }
-        } catch (err) {
-          console.log(err);
-        }
     };
-    
+
     return (
         <div>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} method="POST">
                 <Form.Group class="mb-3">
                     <Form.Label class="form-label">Nome Completo</Form.Label>
                     <Form.Control type="text" class="form-control" id="fullName" aria-describedby="emailHelp" value={fullName} onChange={(e) => setName(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Form.Group class="mb-3">
-                    <Form.Label  class="form-label">Data de nascimento</Form.Label>
+                    <Form.Label class="form-label">Data de nascimento</Form.Label>
                     <Form.Control type="date" id="birthDate" value={birthDate} onChange={(e) => setDate(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </Form.Group>
                 <Form.Group class="mb-3">
-                    <Form.Label  class="form-label">Senha</Form.Label>
+                    <Form.Label class="form-label">Senha</Form.Label>
                     <Form.Control type="password" class="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Form.Label class="form-label">País</Form.Label>
@@ -88,7 +82,7 @@ export default function HomePage() {
                     <option value="2">Two</option>
                     <option value="3">Three</option>
                 </Form.Select>
-                <Form.Control type="text" class="form-control" id="role" hidden={true} value={role}></Form.Control>
+                <Form.Control type="text" class="form-control" id="role" hidden={true} value={role} readOnly={true}></Form.Control>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
