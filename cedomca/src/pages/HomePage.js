3d1b9a6ce37ba0880d1487d57a882cb508c2da07
ingracristina <ingra.cristina@amaro.com>
2022-11-27 13:33:51 -0400

    Layout form

--- cedomca/src/pages/HomePage.js
+++ cedomca/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col, Row } from 'react-bootstrap';
 import { useState } from "react";
 import axios from "axios";
 
@@ -44,45 +44,60 @@ export default function HomePage() {
 
     return (
         <div>
-            <Form onSubmit={handleSubmit} method="POST">
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col>
+                        <Form.Group class="mb-3">
+                            <Form.Label class="form-label">Nome Completo</Form.Label>
+                            <Form.Control type="text" class="form-control" id="fullName" aria-describedby="emailHelp" value={fullName} onChange={(e) => setName(e.target.value)}></Form.Control>
+                        </Form.Group>
+                        <Form.Group class="mb-3">
+                            <Form.Label  class="form-label">Data de nascimento</Form.Label>
+                            <Form.Control type="date" id="birthDate" value={birthDate} onChange={(e) => setDate(e.target.value)}></Form.Control>
+                        </Form.Group>
+                        <Form.Group> 
+                            <Form.Label class="form-label">Estado</Form.Label>
+                            <Form.Select aria-label="Default select example" id="state" value={state} onChange={(e) => setState(e.target.value)}>
+                                <option>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Form.Group>
+                        
+                    </Col>
+                    <Col>                        
+                        
+                        <Form.Group class="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </Form.Group>
+                        <Form.Group class="mb-3">
+                            <Form.Label class="form-label">País</Form.Label>
+                            <Form.Select aria-label="Default select example" id="country" value={country} onChange={(e) => setCountry(e.target.value)}>
+                                <option>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Form.Group>
+                        
+                        <Form.Group class="mb-3">
+                            <Form.Label class="form-label">Cidade</Form.Label>
+                            <Form.Select aria-label="Default select example" id="city" value={city} onChange={(e) => setCity(e.target.value)}>
+                                <option>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Control type="text" class="form-control" id="role" hidden={true} value={role}></Form.Control>
+                    </Col>
+                </Row>
                 <Form.Group class="mb-3">
-                    <Form.Label class="form-label">Nome Completo</Form.Label>
-                    <Form.Control type="text" class="form-control" id="fullName" aria-describedby="emailHelp" value={fullName} onChange={(e) => setName(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group class="mb-3">
-                    <Form.Label class="form-label">Data de nascimento</Form.Label>
-                    <Form.Control type="date" id="birthDate" value={birthDate} onChange={(e) => setDate(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </Form.Group>
-                <Form.Group class="mb-3">
-                    <Form.Label class="form-label">Senha</Form.Label>
-                    <Form.Control type="password" class="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Label class="form-label">País</Form.Label>
-                <Form.Select aria-label="Default select example" id="country" value={country} onChange={(e) => setCountry(e.target.value)}>
-                    <option>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </Form.Select>
-                <Form.Label class="form-label">Estado</Form.Label>
-                <Form.Select aria-label="Default select example" id="state" value={state} onChange={(e) => setState(e.target.value)}>
-                    <option>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </Form.Select>
-                <Form.Label class="form-label">Cidade</Form.Label>
-                <Form.Select aria-label="Default select example" id="city" value={city} onChange={(e) => setCity(e.target.value)}>
-                    <option>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </Form.Select>
-                <Form.Control type="text" class="form-control" id="role" hidden={true} value={role} readOnly={true}></Form.Control>
+                            <Form.Label  class="form-label">Senha</Form.Label>
+                            <Form.Control type="password" class="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                        </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
