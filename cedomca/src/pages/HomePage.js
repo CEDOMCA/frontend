9976f5e75b6e9cbbda3daf00f0a800559cf76137
guilherme <jguicf@gmail.com>
2022-12-02 11:45:50 -0400

    Validation fields complete

--- cedomca/src/pages/HomePage.js
+++ cedomca/src/pages/HomePage.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { Form, Button, Col, Row, Container } from 'react-bootstrap';
+import React, { CSSProperties } from 'react';
 import { Country, State, City } from "country-state-city";
 import Select from "react-select";
 import { useEffect, useState } from "react";
@@ -11,11 +11,47 @@ export default function HomePage() {
     const [birthDate, setDate] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const role = "visitor";
     const [message, setMessage] = useState("");
     const [selectedCountry, setSelectedCountry] = useState(null);
     const [selectedState, setSelectedState] = useState(null);
     const [selectedCity, setSelectedCity] = useState(null);
+    const [ form, setForm ] = useState({})
+    const [ errors, setErrors ] = useState({})
+
+    const setField = (field, value) => {
+        setForm({
+          ...form,
+          [field]: value
+        })
+
+        if ( !!errors[field] ) setErrors({
+            ...errors,
+            [field]: null
+          })
+    }
+
+    const findFormErrors = () => {
+        const { name, birthDate, email, password, confirmPassword} = form
+        const newErrors = {}
+        // name errors
+        if ( !name || name === '' ) newErrors.name = 'Nome obrigatório'
+        // food errors
+        if ( !birthDate || birthDate === '' ) newErrors.birthDate = 'Data de nascimento obrigatório'
+        // rating errors
+        if ( !email || email === '' ) newErrors.email = 'Email obrigatório'
+        // comment errors
+        if ( !password || password === '' ) newErrors.password = 'Senha obrigatório'
+        else if ( password.length > 18 ) newErrors.password = 'Senha muito longa! Sua senha deve conter entre 8 e 18 caracteres'
+        else if ( password.length < 8 ) newErrors.password = 'Senha muito curta! Sua senha deve conter entre 8 e 18 caracteres'
+
+        if ( !confirmPassword || confirmPassword === '' ) newErrors.confirmPassword = 'Confirmar senha obrigatório'
+        else if ( confirmPassword !== password ) newErrors.confirmPassword = 'As senhas devem ser igual'
+    
+        return newErrors
+    }
+    
     useEffect(() => {
         console.log(selectedCountry);
         console.log(selectedCountry?.isoCode);
@@ -24,20 +60,30 @@ export default function HomePage() {
 
     let handleSubmit = async (e) => {
         e.preventDefault();
-        const options = {
-            fullName: fullName,
-            birthDate: birthDate,
-            email: email,
-            password: password,
-            country: selectedCountry.name,
-            state: selectedState.name,
-            city: selectedCity.name,
-            role: role
-        };
-        try {
-            let res = await axios.post("https://cedomca-backend.herokuapp.com/users", options);
-        } catch (err) {
-            console.log(err)
+
+        const newErrors = findFormErrors()
+
+        if ( Object.keys(newErrors).length > 0 ) {
+
+        setErrors(newErrors)
+        } else {
+            const options = {
+                fullName: fullName,
+                birthDate: birthDate,
+                email: email,
+                password: password,
+                country: selectedCountry.name,
+                state: selectedState.name,
+                city: selectedCity.name,
+                role: role
+            };
+            try {
+                
+                let res = await axios.post("https://cedomca-backend.herokuapp.com/users", options);
+                
+            } catch (err) {
+                console.log(err)
+            }
         }
     };
 
@@ -51,11 +97,17 @@ export default function HomePage() {
                     <Col>
                         <Form.Group class="mb-3">
                             <Form.Label class="form-label">Nome Completo</Form.Label>
-                            <Form.Control type="text" class="form-control" id="fullName" aria-describedby="emailHelp" value={fullName} onChange={(e) => setName(e.target.value)}></Form.Control>
+                            <Form.Control type="text" class="form-control" id="fullName" aria-describedby="emailHelp" onChange={(e) => setField('name',e.target.value)} isInvalid={ !!errors.name }></Form.Control>
+                            <Form.Control.Feedback type='invalid'>
+                                { errors.name }
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group class="mb-3">
                             <Form.Label class="form-label">Data de nascimento</Form.Label>
-                            <Form.Control type="date" id="birthDate" value={birthDate} onChange={(e) => setDate(e.target.value)}></Form.Control>
+                            <Form.Control type="date" id="birthDate" onChange={(e) => setField('birthDate',e.target.value)} isInvalid={ !!errors.birthDate }></Form.Control>
+                            <Form.Control.Feedback type='invalid'>
+                                { errors.birthDate }
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label class="form-label">Estado</Form.Label>
@@ -71,6 +123,15 @@ export default function HomePage() {
                                 onChange={(item) => {
                                     setSelectedState(item);
                                 }}
+                                theme={(theme) => ({
+                                    ...theme,
+                                    borderRadius: 0,
+                                    colors: {
+                                      ...theme.colors,
+                                      primary25: 'primary50',
+                                      primary: 'black',
+                                    },
+                                  })}
                             />
                         </Form.Group>
 
@@ -79,7 +140,10 @@ export default function HomePage() {
 
                         <Form.Group class="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <Form.Control type="email" placeholder="name@example.com" onChange={(e) => setField('email',e.target.value)} isInvalid={ !!errors.email }/>
+                            <Form.Control.Feedback type='invalid'>
+                                { errors.email }
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group class="mb-3">
                             <Form.Label class="form-label">País</Form.Label>
@@ -123,18 +187,23 @@ export default function HomePage() {
                 <Row id="passwordBox">
                     <Form.Group class="mb-3">
                         <Form.Label class="form-label">Senha</Form.Label>
-                        <Form.Control type="password" class="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                        <Form.Control type="password" class="form-control" id="password" onChange={(e) => setField('password',e.target.value)} isInvalid={ !!errors.password }></Form.Control>
+                        <Form.Control.Feedback type='invalid'>
+                            { errors.password }
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group class="mb-3">
-                        <Form.Label class="form-label">Confirmar Senha</Form.Label>
-                        <Form.Control type="password" class="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                        <Form.Label class="form-label" >Confirmar Senha</Form.Label>
+                        <Form.Control type="password" class="form-control" id="password" onChange={(e) => setField('confirmPassword',e.target.value)} isInvalid={ !!errors.confirmPassword }></Form.Control>
+                        <Form.Control.Feedback type='invalid'>
+                            { errors.confirmPassword }
+                        </Form.Control.Feedback>
+                        <div class="message">{message ? <p>{message}</p> : null}</div>
                     </Form.Group>
                     <Button variant="primary" type="submit" id="registerButton">
                         Cadastre-se
                     </Button>
                 </Row>
-                
-                <div class="message">{message ? <p>{message}</p> : null}</div>
             </Form>
         </Container>
         </>
