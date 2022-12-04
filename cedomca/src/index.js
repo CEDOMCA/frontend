4879bf82971116460eaad7643fbe184ab626fcb1
guicfreitas <35777831+guicfreitas@users.noreply.github.com>
2022-12-04 16:31:29 -0400

    Feature/fcdom 002/create register screen (#4)
    Co-authored-by: ingracristina <ingra.cristina@amaro.com>

--- cedomca/src/index.js
+++ cedomca/src/index.js
@@ -3,8 +3,21 @@ import { render } from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 render(
   <BrowserRouter>
+    <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">CEDOMCA</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     <App />
   </BrowserRouter>,
   document.querySelector("#root")
