98da0030e739a896c2ed2473a31abb5ec44df734
Ingra Cristina <114255007+ingracristina@users.noreply.github.com>
2022-12-04 16:55:41 -0400

    Feature/fcdom 003/login screen (#2)

--- cedomca/src/App.js
+++ cedomca/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<HomePage/>}/>
+      <Route exact path="/" element={<LoginPage/>}/>
     </Routes>
   )
 }
---
+++ cedomca/src/pages/LoginPage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Form, Button, Row, Container } from 'react-bootstrap';
+import { useState } from "react";
+import axios from "axios";
+import "./LoginPage.css";
+
+export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        const options = {
+            email: email,
+            password: password,
+          };
+          try {
+            let res = await axios.post("https://cedomca-backend.herokuapp.com/auth/login", options);
+          } catch(err) {
+            console.log(err)
+          }
+    };
+
+    return (
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Container bsPrefix="registerContainer">
+                <p class="text-center" id="titletLogin">Login</p>
+                    <Row>
+                        <Form.Group class="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Control type="email" placeholder="nome@exemplo.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group class="mb-3">
+                            <Form.Control type="password" class="form-control" id="password" placeholder="Digite sua senha"
+                            value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                        </Form.Group>
+                    </Row>
+                    
+                    <Row id="rowButton">
+                        <Button variant="primary" type="submit" id="registerButton" >
+                            Entrar
+                        </Button>
+                        
+                    </Row>
+                    <a id="linkRegister" href="/register" class="text-decoration-none">Não possui um cadastro?</a>
+                </Container>
+            </Form>
+        </div>
+    );
+}
