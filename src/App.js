14852212da3d731bf0c83237479d87ffaeab2955
foliveirafilho <franciscofilho1109@gmail.com>
2022-12-05 20:21:11 -0400

    feat(SignUp): new signup template layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
+import SignUpTest from './pages/SignUp/SignUpTest';
 
 export default function App() {
   return (
     <Routes>
       <Route exact path="/" element={<SignIn/>}/>
+      <Route exact path="/test" element={<SignUpTest/>}/>
       <Route exact path="/signup" element={<SignUp/>}/>
     </Routes>
   )
---
+++ src/pages/SignUp/SignUpTest.js
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { useState } from 'react';
+
+const theme = createTheme();
+
+export default function SignUpTest() {
+    const [birthdate, setBirthDate] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Cadastre sua conta
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="fullName"
+                  required
+                  fullWidth
+                  id="fullName"
+                  label="Nome completo"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="E-mail"
+                  name="email"
+                />
+              </Grid>
+              {/* <Grid item xs={12}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DesktopDatePicker
+                        label="Date desktop"
+                        inputFormat="MM/DD/YYYY"
+                        value={birthdate}
+                        onChange={(newBirthDate) => setBirthDate(newBirthDate)}
+                        renderInput={(params) => <TextField {...params} />}
+                        />
+                </LocalizationProvider>
+              </Grid> */}
+              <Grid item xs={12} sm={6}>
+                <Select
+                    labelId="country"
+                    id="country"
+                    value={10}
+                    label="country"
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Select
+                    labelId="state"
+                    id="state"
+                    value={10}
+                    label="state"
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Select
+                    labelId="city"
+                    id="city"
+                    value={10}
+                    label="city"
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Senha"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirmar senha"
+                  type="confirmPassword"
+                  id="confirmPassword"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Cadastrar-se
+            </Button>
+            <Grid container justifyContent="flex-start">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  Já possui uma conta? Faça login
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
