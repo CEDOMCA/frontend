1e8d78ebd689e4caf6277052d9de95d4f5437dcc
ingracristina <ingra.cristina@amaro.com>
2022-12-11 21:29:57 -0400

    add box users

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -12,52 +12,23 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
-
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  })); 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
 
 
 const pages = ['Obras', 'Fontes', 'Usuários'];
@@ -66,6 +37,7 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -78,9 +50,25 @@ function ResponsiveAppBar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+  
+    
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
 
   return (
-    <AppBar position="static">
+    <div>
+<AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
 
@@ -157,19 +145,161 @@ function ResponsiveAppBar() {
               ))}
 
             </Menu>
-            <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Procurar..."
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
           </Box>
         </Toolbar>
       </Container>
+      
     </AppBar>
+    <Paper sx={{ maxWidth: 980, margin: 'auto', marginTop: 5, overflow: 'hidden' }}>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
+      >
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Grid item>
+              <SearchIcon color="inherit" sx={{ display: 'block' }}/>
+            </Grid>
+            <Grid item sm={3.0}>
+              <TextField
+                fullWidth
+                placeholder="Pequisar por nome do usuário"
+                InputProps={{
+                  disableUnderline: true,
+                  sx: { fontSize: 'default' },
+                }}
+                variant="standard"
+              />
+            </Grid>
+            
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center" hidden>
+        Não existe fontes registradas no momento.
+      </Typography>
+      <List alignItems="center" sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <ListItem alignItems="center" secondaryAction={
+                  <Grid container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="flex-start">
+                    <Button size="small" variant="text" startIcon={<DeleteIcon />} color="error">
+                    Excluir usuário
+                    </Button>
+                    <Button size="small" variant="text" startIcon={<EditIcon />} >
+                    Editar usuário
+                    </Button>
+                  </Grid>  
+                  }>
+        <ListItemText
+          primary="Nome:"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                E-mail:
+              </Typography>
+              {" .............."}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider component="li" />
+      <ListItem alignItems="center" secondaryAction={
+                  <Grid container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="flex-start">
+                    <Button size="small" variant="text" startIcon={<DeleteIcon />} color="error">
+                    Excluir usuário
+                    </Button>
+                    <Button size="small" variant="text" startIcon={<EditIcon />} >
+                    Editar usuário
+                    </Button>
+                  </Grid> 
+                  }>
+        <ListItemText
+          primary="Nome:"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                E-mail:
+              </Typography>
+              {" .............."}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider component="li" />
+      <ListItem alignItems="center" secondaryAction={
+                  <Grid container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="flex-start"
+                  spacing={1}>
+                    <Button size="small" variant="text" startIcon={<DeleteIcon />} color="error">
+                    Excluir usuário
+                    </Button>
+                    <Button size="small" variant="text" startIcon={<EditIcon />} >
+                    Editar usuário
+                    </Button>
+                  </Grid>
+                    
+                  }>
+        <ListItemText
+          primary="Nome: ..."
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                E-mail:
+              </Typography>
+              {' ..............'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              Let Google help apps determine location. This means sending anonymous
+              location data to Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Disagree</Button>
+            <Button onClick={handleClose}>Agree</Button>
+          </DialogActions>
+        </Dialog>
+    </Paper>
+    </div>
+    
+    
+    
   );
 }
 export default ResponsiveAppBar;
