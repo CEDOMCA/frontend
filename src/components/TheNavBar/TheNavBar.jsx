e73288c64991613d60d9843332dcb3840deb588c
foliveirafilho <franciscofilho1109@gmail.com>
2022-12-27 13:52:11 -0400

    feat(TheNavBar): make the logo icon clickable

--- src/components/TheNavBar/TheNavBar.jsx
+++ src/components/TheNavBar/TheNavBar.jsx
@@ -1,35 +1,49 @@
 import AdbIcon from '@mui/icons-material/Adb';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Stack, Button, Typography, AppBar, Toolbar } from '@mui/material';
-import React from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/auth';
 import { TheNavBarTabs } from './TheNavBarTabs';
 
 export const TheNavBar = () => {
-  const { authenticated, logout } = React.useContext(AuthContext);
+  const { authenticated, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
   };
 
+  const handleLogoClick = () => {
+    navigate('/');
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <AdbIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 2 }} />
-        <Typography
-          variant="h6"
+        <Stack 
+          direction="row" 
+          onClick={handleLogoClick}
           sx={{
-            mr: 2,
-            display: { xs: 'flex', md: 'flex' },
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none',
+            cursor: 'pointer',
           }}
         >
-          CEDOMCA
-        </Typography>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 2 }} />
+          <Typography
+            variant="h6"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            CEDOMCA
+          </Typography>
+        </Stack>
         {authenticated ? (
           <Stack
             direction="row"
