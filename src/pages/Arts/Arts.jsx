9c0e7ff3ac02fd379d10b68e98dd2ad586c3f277
guilherme <jguicf@gmail.com>
2023-01-03 18:29:49 -0400

    Adding table to arts

--- src/pages/Arts/Arts.jsx
+++ src/pages/Arts/Arts.jsx
@@ -1,8 +1,58 @@
-import { Typography } from '@mui/material';
-//import * as React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+    Paper,
+    AppBar,
+    Toolbar,
+    Stack,
+    TextField,
+    Button
+} from '@mui/material';
+//import { ResourceListItem } from '../../components/ResourceListItem/ResourceListItem';
 
 export default function Arts() {
     return (
-        <Typography>Hello</Typography>
+        <Paper sx={{ maxWidth: 980, margin: 'auto', marginTop: 5, overflow: 'hidden' }}>
+            <AppBar
+                position="static"
+                color="default"
+                elevation={0}
+                sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
+            >
+                <Toolbar>
+                    <Stack
+                        direction="row"
+                        sx={{
+                            flexGrow: 1,
+                        }}
+                    >
+                        <TextField
+                            fullWidth
+                            placeholder="Pesquisar por obras"
+                            InputProps={{
+                                disableUnderline: true,
+                                sx: { fontSize: 'default' },
+                                startAdornment: <SearchIcon color="inherit" sx={{ display: 'block' }} />,
+                            }}
+                            variant="standard"
+                        //onChange={(event) => setSearchString(event.target.value)}
+                        />
+                        <Button
+                            startIcon={<AddIcon />}
+                            disabled={false}
+                            size="small"
+                            variant="contained"
+                            //onClick={handleClickOpen}
+                            sx={{
+                                whiteSpace: 'nowrap',
+                                minWidth: 'max-content',
+                            }}
+                        >
+                            Adicionar nova obra
+                        </Button>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+        </Paper>
     )
 }
