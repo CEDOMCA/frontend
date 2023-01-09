e047b2188021bb6d67902054f56192f3ddf86e2d
fabianabezerraicomp <74303825+fabianabezerraicomp@users.noreply.github.com>
2023-01-09 10:58:16 -0400

    Add confirmation modal on logout (#11)

--- src/components/TheNavBar/TheNavBar.jsx
+++ src/components/TheNavBar/TheNavBar.jsx
@@ -1,14 +1,25 @@
 import AdbIcon from '@mui/icons-material/Adb';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Stack, Button, Typography, AppBar, Toolbar } from '@mui/material';
-import { useContext } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/auth';
 import { TheNavBarTabs } from './TheNavBarTabs';
 
 export const TheNavBar = () => {
   const { authenticated, logout } = useContext(AuthContext);
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleLogout = () => {
+    setOpen(false);
     logout();
   };
 
@@ -54,11 +65,30 @@ export const TheNavBar = () => {
           </Stack>
         ) : null}
         {authenticated ? (
-          <Button color="inherit" onClick={handleLogout} startIcon={<LogoutIcon />}>
+          <Button color="inherit" onClick={handleClickOpen} startIcon={<LogoutIcon />}>
             Logout
           </Button>
         ) : null}
       </Toolbar>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Sair"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Você tem certeza que deseja sair?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleLogout}>OK</Button>
+        </DialogActions>
+      </Dialog>
     </AppBar>
   );
 };
